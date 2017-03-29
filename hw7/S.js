79cc0d243094f1b5878e86f5a57f4fbf1a3aa1af
Aadi Joshi <aadijoshi@hotmail.com>
2017-03-29 13:14:06 -0400

    hw7

--- hw7/S.js
+++ hw7/S.js
@@ -31,9 +31,18 @@ var S = {};
 
    S.spiral = function(u, v){
 		var theta = 2 * Math.PI * u;
+		var x = Math.cos(theta);
+		var y = Math.sin(theta);
+		var x2 = x*x;
+		var y2 = y*y;
+		var r2 = 2;
+		var z2 = r2-x2-y2;
+		var z = Math.sqrt(z2);
+
+
 		return [ Math.cos(theta),
 				   Math.sin(theta),
-			   0 ];
+			   z ];
 		var r2 = 2;
 		var u2 = u*u;
 		var v2 = v*v;
